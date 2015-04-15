99058b56e155a1cf71d66350d4d6b1b49779c68d
eru <eru@dominos.com.au>
2015-04-15 22:16:01 +1000

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+
+$(function(){
+  $('#loading-gif').hide();
+  $('#modal-error').hide();
+  $('.bsod').hide();
+
+  $('.tech-stack').click(function(){
+    $('#loading-gif').show();
+
+    var stackName = $(this).attr('data-name');
+    $.ajax({ url: '/tweets/'+stackName}).done(function(data){
+      $('#results').html('');
+      for(var i = 0; i < data.length; i++){
+        var li = '<li>' + data[i] + '</li>';
+        $('#results').append(li);
+      }
+      $('#loading-gif').hide();
+    }).fail(function(error){
+      $('#modal-error').show();
+      $('#error-sound')[0].play();
+      $('#results').html('');
+      throw "Error: " + error.responseText;
+    });
+  });
+  $('#modal-error').click(function() {
+    $('#beep-sound')[0].play();
+    $('.bsod').show();
+    launchIntoFullscreen($('.bsod')[0]);
+  });
+})
---
+++ routes/tweets.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var Twitter = require('twitter');
+var client = new Twitter({
+  consumer_key: 'BgSNEdoOoXgxg5cCznT6xHiM8',
+  consumer_secret: 'ZL1t2GqInIThCz0DzYqrJKsncI0bD6euURdD94DXWqSRGNRl9R',
+  access_token_key: '3114582198-x9H8o3Sg3VcTkuysTYvvobIdmeH3sSAwjr6XSrz',
+  access_token_secret: 'fqJKqMyQdSCGkMBhUy7BOwnUXyahuGkUTyEzmIAPEAtyT'
+});
+var router = express.Router();
+
+var baseUrl = 'https://api.twitter.com/1.1/search/tweets.json?q='
+/* GET users listing. */
+var filters = {
+
+
+  java : 'java%20code%20SUCKS%20OR%20WTF%20OR%20Stupid%20OR%20FFS-filter:retweets',
+  js :  'javascript%wtf-filter:retweets',
+  css : 'css%20wtf-filter:retweets'
+
+}
+router.get('/:stack', function(req, res) {
+  var filter = filters[req.params.stack];
+  console.log(filter);
+  client.get('search/tweets', {q: filter}, function(error, tweets, response) {
+    if(error){
+      console.error(error);
+      res.status(500).send('Unable to get tweets');
+      return;
+    }
+    var allTweets = [];
+    console.log('got x results:', tweets.statuses.length);
+    for(var i = 0; i < tweets.statuses.length; i++){
+      allTweets.push(tweets.statuses[i].text);
+    }
+    res.send(allTweets);
+  });
+});
+
+module.exports = router;
